7edbb01374eb678ab421103d4f1938837cb04826
Camzouz <camille.confolens@gmail.com>
2017-03-16 17:51:06 +0100

    commit resultat ex08.js

--- ex08.js
+++ ex08.js
@@ -30,4 +30,23 @@ isAllEven([4, 5, 6]);
 isAllEven([4, 4, 6, 8, 10, 12]);
 
 //  écrire votre code sous ce commentaire
+function isAllEven(table) {
+    for (let i = 0; i < table.length; i++) {
+        if (isEven(table[i]) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
+function isEven(nb) {
+    if (nb % 2 == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+console.log(isAllEven([4, 5, 6]));
+console.log(isAllEven([4, 4, 6, 8, 10, 12]));
